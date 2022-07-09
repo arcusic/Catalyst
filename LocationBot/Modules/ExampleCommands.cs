b9307cfca56e9184b434cb3065b0ba73d67d559c
Benjamin Hoch <benjamin.hoch@arcusic.com>
2022-07-09 19:22:08 -0400

    Added Logging Support

--- LocationBot/Modules/ExampleCommands.cs
+++ LocationBot/Modules/ExampleCommands.cs
@@ -1,5 +1,6 @@
-using Discord;
+﻿using Discord;
 using Discord.Commands;
+using LocationBot.Common;
 using RunMode = Discord.Commands.RunMode;
 
 namespace LocationBot.Modules;
@@ -11,6 +12,10 @@ public class ExampleCommands : ModuleBase<ShardedCommandContext>
     [Command("hello", RunMode = RunMode.Async)]
     public async Task Hello()
     {
+        var whiteCheckMark = new Emoji("\u2705");
+        await Logger.Log(LogSeverity.Verbose, "CommandReceived", $"{Context.User.Username} has invoked !hello from the {Context.Channel.Name} channel.");
+        
         await Context.Message.ReplyAsync($"Hello {Context.User.Username}. Nice to meet you!");
+        await Logger.Log(LogSeverity.Verbose, "ResponseSent", $"'Hello { Context.User.Username}. Nice to meet you!' in the {Context.Channel.Name} channel.");
     }
 }
