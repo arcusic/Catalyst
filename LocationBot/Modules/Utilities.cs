283a4fb367616aedeaada262ea5cb9108aa9b532
Benjamin Hoch <48451054+arcusic@users.noreply.github.com>
2022-07-10 12:56:53 -0400

    Basic Health Check complete.

--- LocationBot/Modules/Utilities.cs
+++ LocationBot/Modules/Utilities.cs
@@ -1,9 +1,25 @@
-﻿using Discord;
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.Json;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Discord;
 using Discord.Commands;
+using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Messaging;
 using LocationBot.Common;
 using RunMode = Discord.Commands.RunMode;
 
 namespace LocationBot.Modules;
+
+public class AppSettings
+{
+    public string? KeyVaultName { get; set; }
+    public string? AzureADClientId { get; set; }
+    public string? AzureADTennantId { get; set; }
+    public string? AzureADClientSecret { get; set; }
+}
 
 public class Utilities : ModuleBase<ShardedCommandContext>
 {
@@ -20,5 +36,128 @@ public class Utilities : ModuleBase<ShardedCommandContext>
 
         await Context.Message.ReplyAsync($"Executing infrastructure health check... please wait.");
         await Logger.Log(LogSeverity.Verbose, $"[{Context.Guild.Name}] ResponseSent", $"'Executing infrastructure health check... please wait.' in the {Context.Channel.Name} channel.");
+
+        var jsonString = await File.ReadAllTextAsync("appsettings.json");
+        var appSettings = JsonDocument.Parse(jsonString)!;
+        var keyVaultSettings = appSettings.RootElement.GetProperty("KeyVault").EnumerateObject();
+        var snmpSettings = appSettings.RootElement.GetProperty("SNMP").EnumerateObject();
+        await Logger.Log(LogSeverity.Debug, $"JSONImported", "JSON file has been successfully imported... Processing.");
+
+        string keyVault = keyVaultSettings
+            .Where(onexs => onexs.Name == "KeyVaultName")
+            .Select(onexs => onexs.Value)
+            .FirstOrDefault()
+            .ToString();
+
+        string azureADTennantId = keyVaultSettings
+            .Where(ascended => ascended.Name == "AzureADTennantId")
+            .Select(ascended => ascended.Value)
+            .FirstOrDefault()
+            .ToString();
+
+        string azureADClientId = keyVaultSettings
+            .Where(goblino => goblino.Name == "AzureADClientId")
+            .Select(goblino => goblino.Value)
+            .FirstOrDefault()
+            .ToString();
+
+        string azureADClientSecret = keyVaultSettings
+            .Where(gremlin => gremlin.Name == "AzureADClientSecret")
+            .Select(gremlin => gremlin.Value)
+            .FirstOrDefault()
+            .ToString();
+
+        string upsIPSecret = snmpSettings
+            .Where(jannik => jannik.Name == "UPSIPAddress")
+            .Select(jannik => jannik.Value)
+            .FirstOrDefault()
+            .ToString();
+
+        string upsSnmpCommunity = snmpSettings
+            .Where(tactical => tactical.Name == "UPSCommunity")
+            .Select(tactical => tactical.Value)
+            .FirstOrDefault()
+            .ToString();
+
+        string upsSnmpPort =  snmpSettings
+            .Where(ghxst => ghxst.Name == "UPSSNMPPort")
+            .Select(ghxst => ghxst.Value)
+            .FirstOrDefault()
+            .ToString();
+
+        await Logger.Log(LogSeverity.Debug, $"JSONParsed", "JSON file has been successfully parsed.");
+
+        var secretClient = new SecretClient(new Uri($"https://{keyVault}.vault.azure.net"), new ClientSecretCredential(azureADTennantId, azureADClientId, azureADClientSecret));
+        await Logger.Log(LogSeverity.Debug, "SNMPSecretClientConfigured", $"Configured Azure Key Vault client to connect to {secretClient.VaultUri}.");
+
+
+        var upsIPAddress = secretClient.GetSecret(upsIPSecret);
+        await Logger.Log(LogSeverity.Debug, "SNMPAddressObtained", $"Successfully obtained SNMP Address from Azure Key Vault.");
+
+        var snmpCommunity = secretClient.GetSecret(upsSnmpCommunity);
+        await Logger.Log(LogSeverity.Debug, "SNMPCommunityObtained", $"Successfully obtained SNMP Community from Azure Key Vault.");
+
+        var snmpPort = secretClient.GetSecret(upsSnmpPort);
+        await Logger.Log(LogSeverity.Debug, "SNMPPortObtained", $"Successfully obtained SNMP Port from Azure Key Vault.");
+
+        var tempResult = Messenger.Get(VersionCode.V2,
+            new IPEndPoint(IPAddress.Parse(upsIPAddress.Value.Value), Convert.ToInt32(snmpPort.Value.Value)),
+            new OctetString(snmpCommunity.Value.Value),
+            new List<Variable> { new Variable(new ObjectIdentifier(".1.3.6.1.4.1.850.1.1.3.3.3.1.1.2.2")) },
+            60000);
+        await Logger.Log(LogSeverity.Debug, "UPSTempObtained", $"Successfully obtained Temperature from UPS. {tempResult[0].Data.ToString()}");
+
+        var humResult = Messenger.Get(VersionCode.V2,
+            new IPEndPoint(IPAddress.Parse(upsIPAddress.Value.Value), Convert.ToInt32(snmpPort.Value.Value)),
+            new OctetString(snmpCommunity.Value.Value),
+            new List<Variable> { new Variable(new ObjectIdentifier(".1.3.6.1.4.1.850.1.1.3.3.3.2.1.1.2")) },
+            60000);
+        await Logger.Log(LogSeverity.Debug, "UPSHumidObtained", $"Successfully obtained Humidity from UPS. {humResult[0].Data.ToString()}");
+
+        var capResult = Messenger.Get(VersionCode.V2,
+            new IPEndPoint(IPAddress.Parse(upsIPAddress.Value.Value), Convert.ToInt32(snmpPort.Value.Value)),
+            new OctetString(snmpCommunity.Value.Value),
+            new List<Variable> { new Variable(new ObjectIdentifier(".1.3.6.1.4.1.850.1.1.3.1.3.1.1.1.4.1")) },
+            60000);
+        await Logger.Log(LogSeverity.Debug, "UPSCapObtained", $"Successfully obtained Battery Capacity from UPS. {capResult[0].Data.ToString()}");
+
+        var timeResult = Messenger.Get(VersionCode.V2,
+            new IPEndPoint(IPAddress.Parse(upsIPAddress.Value.Value), Convert.ToInt32(snmpPort.Value.Value)),
+            new OctetString(snmpCommunity.Value.Value),
+            new List<Variable> { new Variable(new ObjectIdentifier(".1.3.6.1.4.1.850.1.1.3.1.3.1.1.1.3.1")) },
+            60000);
+        await Logger.Log(LogSeverity.Debug, "UPSTimeObtained", $"Successfully obtained Runtime from UPS. {timeResult[0].Data.ToString()}");
+
+        //Ping pingSender = new Ping();
+        //PingOptions options = new PingOptions();
+
+        //string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        //byte[] buffer = Encoding.ASCII.GetBytes(data);
+        //int timeout = 120;
+        //PingReply reply = pingSender.Send("x.x.x.x", timeout, buffer, options);
+        //if (reply.Status == IPStatus.Success)
+        //{
+        //    Console.WriteLine("Address: {0}", reply.Address.ToString());
+        //    Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+        //    Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+        //    Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+        //    Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+        //}
+
+        //if (e.Error != null)
+        //{
+        //    Console.WriteLine("Ping failed:");
+        //    Console.WriteLine(e.Error.ToString());
+
+        //    // Let the main thread resume.
+        //    ((AutoResetEvent)e.UserState).Set();
+        //}
+
+        await Context.Message.ReplyAsync($"__**Network Enclosure Health Report:**__" +
+            $"\n`Current Temperature:` {Convert.ToDecimal(tempResult[0].Data.ToString()) / 10} F\n" +
+            $"`Current Humidity:` {humResult[0].Data}%\n" +
+            $"`UPS Battery Capacity:` {capResult[0].Data}%\n" +
+            $"`UPS Runtime:` {timeResult[0].Data} minutes");
+        await Logger.Log(LogSeverity.Verbose, $"[{Context.Guild.Name}] ResponseSent", $"Health Report sent to the {Context.Channel.Name} channel.");
     }
 }
