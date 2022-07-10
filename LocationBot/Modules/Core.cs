3e4d3bf307f85bcc2add3448f8d29bb416c8a8f2
Benjamin Hoch <benjamin.hoch@arcusic.com>
2022-07-10 16:42:45 -0400

    Corrected Typo

--- LocationBot/Modules/Core.cs
+++ LocationBot/Modules/Core.cs
@@ -18,15 +18,18 @@ public class Core : ModuleBase<ShardedCommandContext>
 
         await Context.Message.AddReactionAsync(whiteCheckMark);
         await Logger.Log(LogSeverity.Verbose, $"[{Context.Guild.Name}] MessageAcknowledged", $"Reacted with :white_check_mark: to {Context.User.Username}#{Context.User.DiscriminatorValue}'s message.");
-        
+
+        string osEmote = Environment.Is64BitOperatingSystem ? ":white_check_mark:" : ":x:";
+        string procEmote = Environment.Is64BitProcess ? ":white_check_mark:" : ":x:";
+
         await Context.Message.ReplyAsync($"\n__**About [PLACEHOLDER]**__\n" +
             $":warning: `THIS IS A PRE-RELEASE VERSION.` :warning:\n\n" +
             $"`[PLACEHOLDER] Version:`  Alpha Development Release v0.1 (Build 2207)\n\n" +
             $"__*System Information*__\n" +
             $"`Active Node:`  {Environment.MachineName}\n" +
             $"`Operating System Version:`  {Environment.OSVersion}\n" +
-            $"`64 Bit Operating System:`  {Environment.Is64BitOperatingSystem}\n" +
-            $"`64 Bit Process:`  {Environment.Is64BitProcess}\n" +
+            $"`64 Bit Operating System:`  {osEmote}\n" +
+            $"`64 Bit Process:`  {procEmote}\n" +
             $"`.NET Version:`  {Environment.Version}\n\n" +
             $"__*Created By:*__\n" +
             $"> Catalyst#7894\n" +
@@ -37,19 +40,32 @@ public class Core : ModuleBase<ShardedCommandContext>
             $"> Utilities Module - v0.1 (Build 2207)\n\n" +
             $"__*Documentation*__\n" +
             $"> Are you kidding... [PLACEHOLDER]\n" +
-            $"> The Change Log can be accessed by executing `!changelog`");
-        await Logger.Log(LogSeverity.Verbose, $"[{Context.Guild.Name}] ResponseSent", $"'Application Version information sent to the {Context.Channel.Name} channel.");
+            $"> The Change Log can be accessed by executing `!changelog`\n" +
+            $"> A list of all commands can be accessed by executing `!help`");
+        await Logger.Log(LogSeverity.Verbose, $"[{Context.Guild.Name}] ResponseSent", $"Application Version information sent to the {Context.Channel.Name} channel.");
     }
     
     [Command("changelog", RunMode = RunMode.Async)]
     public async Task ChangeLog()
     {
         var whiteCheckMark = new Emoji("\u2705");
-        var noEntry = new Emoji("\u274C");
+        var redX = new Emoji("\u274C");
 
         await Logger.Log(LogSeverity.Verbose, $"[{Context.Guild.Name}] CommandReceived", $"{Context.User.Username}#{Context.User.DiscriminatorValue} has invoked {Context.Message.Content} from the {Context.Channel.Name} channel.");
 
-        await Context.Message.AddReactionAsync(noEntry);
-        await Logger.Log(LogSeverity.Verbose, $"[{Context.Guild.Name}] MessageAcknowledged", $"Reacted with :white_check_mark: to {Context.User.Username}#{Context.User.DiscriminatorValue}'s message.");
+        await Context.Message.AddReactionAsync(redX);
+        await Logger.Log(LogSeverity.Verbose, $"[{Context.Guild.Name}] MessageAcknowledged", $"Reacted with :x: to {Context.User.Username}#{Context.User.DiscriminatorValue}'s message.");
+    }
+
+    [Command("help", RunMode = RunMode.Async)]
+    public async Task Help()
+    {
+        var whiteCheckMark = new Emoji("\u2705");
+        var redX = new Emoji("\u274C");
+
+        await Logger.Log(LogSeverity.Verbose, $"[{Context.Guild.Name}] CommandReceived", $"{Context.User.Username}#{Context.User.DiscriminatorValue} has invoked {Context.Message.Content} from the {Context.Channel.Name} channel.");
+
+        await Context.Message.AddReactionAsync(redX);
+        await Logger.Log(LogSeverity.Verbose, $"[{Context.Guild.Name}] MessageAcknowledged", $"Reacted with :x: to {Context.User.Username}#{Context.User.DiscriminatorValue}'s message.");
     }
 }
