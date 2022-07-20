a0ae4d8f53a33c602e6dc33e4af5f3b96da6eee6
Benjamin Hoch <48451054+arcusic@users.noreply.github.com>
2022-07-20 14:49:38 -0400

    Corrected Typo

--- Catalyst/Services/CommandHandler.cs
+++ Catalyst/Services/CommandHandler.cs
@@ -126,7 +126,7 @@ public class CommandHandler : ICommandHandler
                 Author = new EmbedAuthorBuilder
                 {
                     Name = "The Catalyst",
-                    IconUrl = https://user-images.githubusercontent.com/48451054/179251245-db22826b-45fa-47b7-ad30-596678d19c63.png"
+                    IconUrl = "https://user-images.githubusercontent.com/48451054/179251245-db22826b-45fa-47b7-ad30-596678d19c63.png"
                 }
             };
 
