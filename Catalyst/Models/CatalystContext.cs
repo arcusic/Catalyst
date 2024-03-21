f7d7b6eaa0505a1664f93e462ec7c33b2f343598
Benjamin Hoch <48451054+arcusic@users.noreply.github.com>
2024-03-20 21:30:56 -0400

    Enabled Azure Key Vault Support

--- Catalyst/Models/CatalystContext.cs
+++ Catalyst/Models/CatalystContext.cs
@@ -9,10 +9,16 @@ public partial class CatalystContext : DbContext
     public CatalystContext()
     {
     }
+    private readonly string _connectionString;
 
-    public CatalystContext(DbContextOptions<CatalystContext> options)
-        : base(options)
+    public CatalystContext(string connectionString)
     {
+        _connectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_connectionString);
     }
 
     public virtual DbSet<TblAccount> TblAccounts { get; set; }
@@ -20,10 +26,6 @@ public partial class CatalystContext : DbContext
     public virtual DbSet<TblBan> TblBans { get; set; }
 
     public virtual DbSet<TblServer> TblServers { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
